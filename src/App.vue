18a8cb60a0dce429ba7b2cafe482546482702247
SWXDC <xuczxc100@gmail.com>
2025-06-15 06:26:52 +0000

    main

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,113 @@
 <template>
-  <nav class="navbar">
-    <div>
-      <a href="/">
-          <img src="/public/middlePNG.png" alt="">
-          <h1>StrawMinecraftLauncher</h1>
-      </a>
-    </div>
-  </nav>
+  <navbar_vue />
   <main class="main">
-    <section>
-
+    <section class="hero">
+      <div>
+        <div class="hero-text">
+          <h2>開源Mineraft啟動器</h2>
+          <p>新時代Minecraft啟動器，現代介面、跨平台應用</p>
+        </div>
+        <div class="hero-btn">
+          <a href="/download">啟動器下載</a>
+          <a href="https://github.com/StrawCoding/smcl-web" target="_blank">Github</a>
+        </div>
+      </div>
     </section>
     <section></section>
   </main>
+  
 </template>
 <style scoped>
 *{
   margin: 0px;
   padding: 0px;
   box-sizing: border-box;
-  font-family: 'Arial', sans-serif;
-}
-.navbar{
-  display: flex;
-  align-items: center;
-  position: fixed;
-  background-color: #1b1b1f;
-  top: 0px;
-  left: 0px;
-  width: 100dvw;
-  height: 64px;
-  padding: 10px 15px;
-
-  &>div>a{
-    display: flex;
-    text-decoration: none;
-
-    img{
-      height: 40px;
-      border-radius: 10px;
-    }
-
-    h1{
-      margin-left: 10px;
-      color: #B0BEC5;
-      align-self: center;
-    }
-  }
+  font-family: "Noto Sans TC", serif;
 }
 .main{
-  
   section{
+    background-color: #000000;  
+
     &:nth-child(1){
-      padding-top: 70px;
+      padding-top: 80px;
+      height: 100dvh;
+      display: flex;
+      justify-content: center;
+
+      .hero-text{
+        justify-items: center;
+        margin-top: 100px;
+
+        h2{
+          font-size: 48px;
+          display: flex;
+          color: #FFFFFF;
+        }
+        p{
+          font-size: 28px;
+          display: flex;
+          color: #FFFFFF;
+        }
+
+      }
+      .hero-btn{
+        margin-top: 20px;
+        display: flex;
+        justify-content: space-around ;
+        a{
+          transition: all 300ms;
+          color: #DCE2E5;
+          text-decoration: none;
+          padding: 10px 20px;
+          border-radius: 15px;
+          border: 2px solid;
+
+
+          &:nth-child(1){
+            border-color: #39A9DB;
+            &:hover{
+              background-color: #39A9DB;
+              color: #000000;
+            }
+          }
+          &:nth-child(2){
+            border-color: #FDE047;
+            &:hover{
+              background-color: #FDE047;
+              color: #000000;
+            }
+          }
+        }
+      }
+
     }
+  }
+  /* 
     &:nth-child(2n+1){
       background-color: #17232E;
     }
     &:nth-child(2n){
       background-color: #131B23;
     }
-  }
+  } */
 }
 
 
 
 </style>
 <script>
+import navbar_vue from "./components/navbar.vue";
 
 export default{
   name : "App",
-  
+
+  components: {
+    navbar_vue,
+  },
+
   data(){
     return{
-      
+
     }
   }
 }
---
+++ src/components/navbar.vue
@@ -0,0 +1,74 @@
+<template>
+<nav class="navbar">
+    <div class="logo">
+      <a href="/">
+          <img src="/public/middlePNG.png" alt="">
+          <h1>StrawMinecraftLauncher</h1>
+      </a>
+    </div>
+    <div class="navbar-links">
+      <a href="/">🏠首頁</a>
+      <a href="/download">📩下載</a>
+    </div>
+  </nav>
+</template>
+<style scoped>
+*{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  font-family: "Noto Sans TC", serif;
+}
+.navbar{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100dvw;
+  height: 70px;
+  padding: 10px 15px;
+  backdrop-filter: blur(10px);
+
+  .logo>a{
+    display: flex;
+    text-decoration: none;
+
+    img{
+      height: 48px;
+      border-radius: 10px;
+    }
+
+    h1{
+      font-size: 30px ;
+      margin-left: 10px;
+      color: #B0BEC5;
+      align-self: center;
+    }
+  }
+  .navbar-links{
+    display: flex;
+    a{
+      transition: all 200ms;
+      color: #FFFFFF;
+      text-decoration: none;
+      margin: 0px 15px;
+      font-size: 18px;
+      display: flex;
+      align-items: center;
+
+      &:hover{
+        color: #3DB8A5;
+      }
+    }
+  }
+
+}
+</style>
+<script>
+
+export default{
+    name:"navbar",
+}
+</script>
