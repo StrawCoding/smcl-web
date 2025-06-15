505493f05fae7135505de4e5447afc84ad98e4a6
SWXDC <xuczxc100@gmail.com>
2025-06-15 11:32:42 +0000

    main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/main.js'
-import { FontAwesomeIcon } from './plugins/fontawesome'
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
-
