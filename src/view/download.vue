194dc9086c9db5fc36c9757eea15bf73051aa314
SWXDC <xuczxc100@gmail.com>
2025-06-19 14:51:32 +0000

    初始化部件

--- src/view/download.vue
+++ src/view/download.vue
@@ -1,16 +1,31 @@
 <template>
   <navbar_vue />
+  <main class="main">
+    <section></section>
+  </main>
+  <footer_vue />
 </template>
 <style scoped>
+.main{
+  section{
+    background-color: #000000;
 
+    &:nth-child(1){
+      padding-top: 80px;
+      height: 100dvh;
+    }
+  }
+}
 </style>
 <script>
 import navbar_vue from "../components/navbar.vue"
+import footer_vue from "../components/footer.vue"
 
 export default{
   name:"downlaod",
   components: {
     navbar_vue,
+    footer_vue
   },
   data(){
     return{
