1525d0ff7c01b68828195ac4f64ab9b39ea38439
SWXDC <xuczxc100@gmail.com>
2025-06-20 14:20:55 +0000

    修改：更好的img位置

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,7 +2,7 @@
 <nav class="navbar">
     <div class="logo">
       <a href="/">
-          <img src="/public/img/middlePNG.svg" alt="">
+          <img src="/src/assets/img/middlePNG.svg" alt="">
           <h1>{{ web_title }}</h1>
       </a>
     </div>
